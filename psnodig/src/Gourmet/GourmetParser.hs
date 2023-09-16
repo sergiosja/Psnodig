6ef6273bb9ab84d1f55811719540213e85a0a251
Sergey <thefanthomasfan@gmail.com>
2023-09-16 23:59:05 +0200

    renamed gourmet writer/transpiler, and matched parser and writer with new syntax

--- psnodig/src/Gourmet/GourmetParser.hs
+++ psnodig/src/Gourmet/GourmetParser.hs
@@ -3,8 +3,9 @@ module Gourmet.GourmetParser (parseGourmet) where
 import Syntax
 import Text.Parsec
 import Text.Parsec.String
-import qualified Text.Parsec.Token as Token
 import Text.Parsec.Language
+import Text.Parsec.Expr (buildExpressionParser, Assoc(..), Operator(..))
+import qualified Text.Parsec.Token as Token
 
 lexer :: Token.TokenParser ()
 lexer = Token.makeTokenParser emptyDef {
@@ -13,6 +14,8 @@ lexer = Token.makeTokenParser emptyDef {
            Token.reservedOpNames =
             [ ":=", "+", "-", "*", "/", "<", ">", "=="
             , "!=", "fmt.Println", "while", "{", "}", "if"
+            , "pass", "func", "(", ")", ">=", "<=", "true"
+            , "false", "return", "[", "]"
             ]
        }
 
@@ -31,50 +34,88 @@ integer = Token.integer lexer
 parens :: Parser Expression -> Parser Expression
 parens = Token.parens lexer
 
-parseOp :: Parser Operator
-parseOp = choice
-    [ reservedOp "+" >> return Plus
-    , reservedOp "-" >> return Minus
-    , reservedOp "*" >> return Times
-    , reservedOp "/" >> return Division
-    , reservedOp "<" >> return LessThan
-    , reservedOp ">" >> return GreaterThan
-    , reservedOp "==" >> return Equal
-    , reservedOp "!=" >> return NotEqual
-    ]
-
-parseVariableExp :: Parser Expression
-parseVariableExp = do
-    var <- identifier
-    return (VariableExp var)
-
-parseConstant :: Parser Expression
-parseConstant = do
-    value <- integer
-    return (Constant (fromIntegral value))
-
-parseBinaryExp :: Parser Expression
-parseBinaryExp = try $ do
-    expr <- parseTerm
-    binop <- parseOp
-    expr' <- parseExpr
-    return (BinaryExp binop expr expr')
-
-parseTerm :: Parser Expression
-parseTerm = try parseVariableExp <|> parseConstant
+{- Parsing -}
 
 parseExpr :: Parser Expression
-parseExpr = choice
-    [ parseBinaryExp
-    , try parseVariableExp
-    , parseConstant
-    ]
+parseExpr = buildExpressionParser table term
+    where
+        table = [ [ Infix (reservedOp "*" >> return (BinaryExp Times)) AssocLeft
+                    , Infix (reservedOp "/" >> return (BinaryExp Division)) AssocLeft]
+                , [ Infix (reservedOp "+" >> return (BinaryExp Plus)) AssocLeft
+                    , Infix (reservedOp "-" >> return (BinaryExp Minus)) AssocLeft]
+                , [ Infix (reservedOp "==" >> return (BinaryExp Equal)) AssocNone
+                    , Infix (reservedOp "!=" >> return (BinaryExp NotEqual)) AssocNone
+                    , Infix (reservedOp "<" >> return (BinaryExp LessThan)) AssocNone
+                    , Infix (reservedOp "<=" >> return (BinaryExp LessThanEqual)) AssocNone
+                    , Infix (reservedOp ">" >> return (BinaryExp GreaterThan)) AssocNone
+                    , Infix (reservedOp ">=" >> return (BinaryExp GreaterThanEqual)) AssocNone]
+                ]
+        term = choice
+            [ try parseArray
+            , try parseArrayExp
+            , try parseFunctionCallExp
+            , try parseBool
+            , try parseVariableExp
+            , try parseConstant
+            , try $ parens parseExpr
+            ]
+            where
+                parseArray = do
+                    reservedOp "["
+                    entries <- parseExpr `sepBy` (whiteSpace >> char ',' >> whiteSpace)
+                    reservedOp "]"
+                    return $ Array entries
+                parseArrayExp = do
+                    array <- identifier
+                    reservedOp "["
+                    index <- parseExpr
+                    reservedOp "]"
+                    return $ ArrayExp array index
+                parseFunctionCallExp = do
+                    functioncall <- parseFunctionCall
+                    return $ Call functioncall
+                parseVariableExp = do
+                    var <- identifier
+                    return (VariableExp var)
+                parseConstant = do
+                    value <- integer
+                    return (Constant (fromIntegral value))
+                parseBool = parseTrue <|> parseFalse
+                    where
+                        parseTrue = do
+                            reservedOp "true"
+                            return (Boolean True)
+                        parseFalse = do
+                            reservedOp "false"
+                            return (Boolean False)
+
+parseFunction :: Parser Function
+parseFunction = do
+    reservedOp "func"
+    funcname <- identifier
+    reservedOp "("
+    args <- identifier `sepBy` (whiteSpace >> char ',' >> whiteSpace)
+    reservedOp ")"
+    reservedOp "{"
+    stmts <- parseStmt `endBy` whiteSpace
+    reservedOp "}"
+    return $ Function funcname args stmts
+
+parseFunctionCall :: Parser FunctionCall
+parseFunctionCall = do
+    funcname <- identifier
+    reservedOp "("
+    args <- parseExpr `sepBy` (whiteSpace >> char ',' >> whiteSpace)
+    reservedOp ")"
+    return (FunctionCall funcname args)
 
 parseStmt :: Parser Statement
 parseStmt = choice
     [ try parseAssignment
     , try loopStmt
     , try ifStmt
+    , try passStmt
+    , try returnStmt
     , printStmt
     ]
     where
@@ -87,16 +128,23 @@ parseStmt = choice
             reservedOp "while"
             cond <- parseExpr
             reservedOp "{"
-            statements <- many parseStmt
+            statements <- many1 parseStmt
             reservedOp "}"
             return (Loop cond statements)
         ifStmt = do
             reservedOp "if"
             cond <- parseExpr
             reservedOp "{"
-            statements <- many parseStmt
+            statements <- many1 parseStmt
             reservedOp "}"
             return (If cond statements)
+        passStmt = do
+            reservedOp "pass"
+            return Pass
+        returnStmt = do
+            reservedOp "return"
+            expr <- parseExpr
+            return (Return expr)
         printStmt = do
             reservedOp "fmt.Println"
             expr <- parens parseExpr
@@ -105,5 +153,6 @@ parseStmt = choice
 parseGourmet :: Parser Program
 parseGourmet = do
     whiteSpace
-    assignments <- parseStmt `endBy` whiteSpace
-    return (Program assignments)
+    funcs <- many1 {- - -} parseFunction -- <* whiteSpace)
+    functioncall <- parseFunctionCall
+    return $ Program funcs functioncall
---
+++ psnodig/src/Gourmet/GourmetWriter.hs
@@ -0,0 +1,106 @@
+module Gourmet.GourmetWriter (writeGourmet) where
+
+import Control.Monad.Writer
+import Syntax
+
+type GourmetWriter = Writer String
+
+-- Helper methods
+
+addIndents :: Int -> String
+addIndents n = replicate n '\t'
+
+-- Writer
+
+writeExp :: Expression -> GourmetWriter ()
+writeExp (Constant n) = tell $ show n
+writeExp (VariableExp var) = tell var
+writeExp (Boolean val) = case val of
+    True -> tell "true"
+    False -> tell "false"
+writeExp (BinaryExp op exp1 exp2) = do
+    writeExp exp1
+    writeOp op
+    writeExp exp2
+writeExp (Call functioncall) = do
+    writeFunctionCall functioncall
+writeExp (Array entries) = do
+    tell "["
+    case length entries of
+        0 -> tell "]"
+        1 -> (writeExp $ head entries) >> tell "]"
+        _ -> do
+            mapM_ (\x -> (writeExp x) >> tell ", ") (init entries)
+            (writeExp $ last entries) >> tell "]"
+writeExp (ArrayExp name index) = do
+    tell $ name ++ "["
+    writeExp index
+    tell "]"
+
+writeFunctionCall :: FunctionCall -> GourmetWriter ()
+writeFunctionCall (FunctionCall funcname args) = do
+    tell $ funcname ++ "("
+    case length args of
+        0 -> tell ")"
+        1 -> (writeExp $ head args) >> tell ")"
+        _ -> do
+            mapM_ (\arg -> (writeExp arg) >> tell ", ") (init args)
+            (writeExp $ last args) >> tell ")"
+
+
+writeStmt :: Statement -> Int -> GourmetWriter ()
+writeStmt (Assignment var expr) _ = do
+    tell var
+    tell " := "
+    writeExp expr
+writeStmt (Loop expr stmts) indent = do
+    tell "while "
+    writeExp expr
+    tell " {\n"
+    mapM_ (\stmt -> (tell $ addIndents $ indent+1) >> writeStmt stmt (indent+1) >> tell "\n") stmts
+    tell $ (addIndents indent) ++ "}"
+writeStmt (If expr stmts) indent = do
+    tell "if "
+    writeExp expr
+    tell " {\n"
+    mapM_ (\stmt -> (tell $ addIndents $ indent+1) >> writeStmt stmt (indent+1) >> tell "\n") stmts
+    tell $ (addIndents indent) ++ "}"
+writeStmt Pass _ = do
+    tell "pass"
+writeStmt (Return expr) _ = do
+    tell "return "
+    writeExp expr
+writeStmt (Print expr) _ = do
+    tell "fmt.Println("
+    writeExp expr
+    tell ")"
+
+writeFunc :: Function -> GourmetWriter ()
+writeFunc (Function funcname args stmts) = do
+    tell $ "func " ++ funcname ++ "("
+    case length args of
+        0 -> tell ") {\n"
+        1 -> tell $ head args ++ ") {\n"
+        _ -> do
+            mapM_ (\a -> (tell a >> tell ", ")) (init args)
+            tell $ last args ++ ") {\n"
+    mapM_ (\stmt -> tell "\t" >> writeStmt stmt 1 >> tell "\n") stmts
+    tell "}"
+
+
+writeOp :: Operator -> GourmetWriter ()
+writeOp Plus = tell " + "
+writeOp Minus = tell " - "
+writeOp Times = tell " * "
+writeOp Division = tell " / "
+writeOp LessThan = tell " < "
+writeOp LessThanEqual = tell " <= "
+writeOp GreaterThan = tell " > "
+writeOp GreaterThanEqual = tell " >= "
+writeOp Equal = tell " == "
+writeOp NotEqual = tell " != "
+
+writeGourmet :: Program -> GourmetWriter ()
+writeGourmet (Program funcs funcCall) = do
+    mapM_ (\f -> (writeFunc f) >> tell "\n\n") funcs
+    writeFunctionCall funcCall
