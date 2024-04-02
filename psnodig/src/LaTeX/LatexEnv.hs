37a45db30514e46c4d75499b6d24b7b3340e0d61
sergiosja <sergey.jakobsen@gmail.com>
2024-04-02 17:27:37 +0200

    refaktorert preprosessoren til TBP writeren

--- psnodig/src/LaTeX/LatexEnv.hs
+++ psnodig/src/LaTeX/LatexEnv.hs
@@ -6,7 +6,9 @@ import qualified Data.Set as Set
 import qualified Data.Map as Map
 import Syntax
 
-type Collector = State (Set String, Set String, Set String)
+type Collector = State (Set String, Set String)
+
+-- Top levels
 
 collectNames :: Program -> Collector ()
 collectNames (Program structs funcs _) = do
@@ -15,95 +17,108 @@ collectNames (Program structs funcs _) = do
 
 collectStructDeclarations :: StructDecl -> Collector ()
 collectStructDeclarations (StructDecl name _) = do
-    modify(\(structs, funcs, lists) -> (Set.insert name structs, funcs, lists))
+    modify(\(funcs, keywords) -> (Set.insert name funcs, keywords))
 
 collectFuncDeclarations :: Function -> Collector ()
-collectFuncDeclarations (Function name args stmts) = do
-    modify (\(structs, funcs, lists) -> (structs, Set.insert name funcs, lists))
-    mapM_ collectArgs args
-    mapM_ collectStmts stmts
+collectFuncDeclarations (Function name _ stmts) = do
+    modify (\(funcs, keywords) -> (Set.insert name funcs, keywords))
+    mapM_ collectStmt stmts
 
-collectArgs :: Argument -> Collector ()
-collectArgs (Argument n t) = case t of
-    "bool" -> return ()
-    "int" -> return ()
-    "string" -> return ()
-    _ ->
-        modify (\(structs, funcs, lists) -> (structs, funcs, Set.insert n lists))
+collectFunctionCall :: FunctionCall -> Collector ()
+collectFunctionCall (FunctionCall name exprs) = do
+    modify (\(funcs, keywords) -> (Set.insert name funcs, keywords))
+    mapM_ collectExpr exprs
 
-collectStmts :: Statement -> Collector ()
-collectStmts stmt =
-    case stmt of
-        (Assignment (VariableTarget name) (ExpressionValue (Constant value))) -> do
-            modify (\(structs, funcs, lists) -> (structs, funcs, Set.insert name lists))
-            case value of
-                (List exprs) -> mapM_ collectExpr exprs
-                (HashSet set) ->
-                    let exprs = Set.toList set
-                    in mapM_ collectExpr exprs
-                (HashMap hmap) ->
-                    let pairs = Map.toList hmap
-                    in mapM_ collectPair pairs
-                _ -> return ()
-        (Assignment (VariableTarget _) (ExpressionValue expr)) ->
-            collectExpr expr
-        (Loop expr stmts) -> do
-            collectExpr expr
-            mapM_ collectStmts stmts
-        (If expr stmts maybeElse) -> do
-            collectExpr expr
-            mapM_ collectStmts stmts
-            collectElse maybeElse
-        (ForEach _ expr stmts) -> do
-            collectExpr expr
-            mapM_ collectStmts stmts
-        (For _ expr1 expr2 stmts) -> do
-            collectExpr expr1
-            collectExpr expr2
-            mapM_ collectStmts stmts
-        (CallStmt (FunctionCall name _)) ->
-            modify (\(structs, funcs, lists) -> (structs, Set.insert name funcs, lists))
-        (Return expr) ->
-            collectExpr expr
-        (HashStmt stmnt) ->
-            collectStmts stmnt
-        (AnnotationStmt _ stmts) ->
-            mapM_ collectStmts stmts
-        _ -> return ()
+-- Statements
+
+collectStmt :: Statement -> Collector ()
+collectStmt (Assignment target value) = do
+    collectAssignmentTarget target
+    collectAssignmentValue value
+collectStmt (Loop expr stmts) = do
+    collectExpr expr
+    mapM_ collectStmt stmts
+collectStmt (If expr stmts maybeElse) = do
+    collectExpr expr
+    mapM_ collectStmt stmts
+    collectMaybeElse maybeElse
+collectStmt (ForEach _ expr stmts) = do
+    collectExpr expr
+    mapM_ collectStmt stmts
+collectStmt (For _ expr1 expr2 stmts) = do
+    modify (\(funcs, keywords) -> (funcs, Set.insert "to" keywords))
+    collectExpr expr1
+    collectExpr expr2
+    mapM_ collectStmt stmts
+collectStmt (CallStmt functionCall) = collectFunctionCall functionCall
+collectStmt (Return expr) =
+    collectExpr expr
+collectStmt (HashStmt stmnt) =
+    collectStmt stmnt
+collectStmt (AnnotationStmt _ stmts) =
+    mapM_ collectStmt stmts
+collectStmt Break =
+    modify (\(funcs, keywords) -> (funcs, Set.insert "break" keywords))
+collectStmt Continue =
+    modify (\(funcs, keywords) -> (funcs, Set.insert "continue" keywords))
+
+collectAssignmentTarget :: AssignmentTarget -> Collector ()
+collectAssignmentTarget (ListIndexTarget _ exprs) =
+    mapM_ collectExpr exprs
+collectAssignmentTarget _ = return ()
+
+collectAssignmentValue :: AssignmentValue -> Collector ()
+collectAssignmentValue (ExpressionValue expr) =
+    collectExpr expr
+collectAssignmentValue _ = return ()
+
+collectMaybeElse :: Maybe Else -> Collector ()
+collectMaybeElse (Just (ElseIf expr stmts maybeElse)) = do
+    collectExpr expr
+    mapM_ collectStmt stmts
+    collectMaybeElse maybeElse
+collectMaybeElse (Just (Else stmts)) = do
+    mapM_ collectStmt stmts
+collectMaybeElse Nothing = return ()
+
+-- Expressions and Values
 
 collectExpr :: Expression -> Collector ()
-collectExpr arg =
-    case arg of
-        (BinaryExp _ exp1 exp2) -> do
-            collectExpr exp1
-            collectExpr exp2
-        (ListIndex list exprs) -> do
-            modify (\(structs, funcs, lists) -> (structs, funcs, Set.insert list lists))
-            mapM_ collectExpr exprs
-        (CallExp (FunctionCall name exps)) -> do -- if f = contains(A, x), add A to lists
-            modify (\(structs, funcs, lists) -> (structs, Set.insert name funcs, lists))
-            mapM_ collectExpr exps
-        (Not expr) ->
-            collectExpr expr
-        _ -> return ()
+collectExpr (Constant value) = collectValue value
+collectExpr (BinaryExp _ expr1 expr2) = do
+    collectExpr expr1
+    collectExpr expr2
+collectExpr (ListIndex _ exprs) =
+    mapM_ collectExpr exprs
+collectExpr (CallExp functionCall) = collectFunctionCall functionCall
+collectExpr (Not expr) = do
+    modify (\(funcs, keywords) -> (funcs, Set.insert "not" keywords))
+    collectExpr expr
+collectExpr _ = return ()
 
-collectElse :: Maybe Else -> Collector ()
-collectElse elsePart =
-    case elsePart of
-        (Just (ElseIf expr stmts maybeElse)) -> do
-            collectExpr expr
-            mapM_ collectStmts stmts
-            collectElse maybeElse
-        (Just (Else stmts)) ->
-            mapM_ collectStmts stmts
-        Nothing -> return ()
+collectValue :: Value -> Collector ()
+collectValue Nil =
+    modify (\(funcs, keywords) -> (funcs, Set.insert "Nil" keywords))
+collectValue (Boolean True) =
+    modify (\(funcs, keywords) -> (funcs, Set.insert "true" keywords))
+collectValue (Boolean False) =
+    modify (\(funcs, keywords) -> (funcs, Set.insert "false" keywords))
+collectValue (List exprs) =
+    mapM_ collectExpr exprs
+collectValue (HashSet set) =
+    let exprs = Set.toList set in mapM_ collectExpr exprs
+collectValue (HashMap hmap) =
+    let pairs = Map.toList hmap in mapM_ collectPair pairs
+collectValue _ = return ()
 
 collectPair :: (Expression, Expression) -> Collector ()
 collectPair (x, y) = do
     collectExpr x
     collectExpr y
 
-extractEnv :: Program -> ([String], [String], [String])
+-- Entry point
+
+extractEnv :: Program -> ([String], [String])
 extractEnv program =
-    let (structs, funcs, lists) = execState (collectNames program) (Set.empty, Set.empty, Set.empty)
-    in (Set.toList structs, Set.toList funcs, Set.toList lists)
+    let (funcs, keywords) = execState (collectNames program) (Set.empty, Set.empty)
+    in (Set.toList funcs, Set.toList keywords)
