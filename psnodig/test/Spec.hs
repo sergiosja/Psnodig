95e48e24bc027c2554c73d0bcea6baaa29bcb986
sergiosja <sergey.jakobsen@gmail.com>
2024-05-03 21:56:35 +0200

    started on test spec

--- psnodig/test/Spec.hs
+++ psnodig/test/Spec.hs
@@ -1,2 +1,34 @@
+import Parsers.Gourmet (testGourmetParser)
+import Writers.Gourmet (testGourmetWriter)
+import Writers.Pytite (testPytiteWriter)
+import Writers.Pseudocode (testPseudocodeWriter)
+import Test.HUnit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    putStrLn "\n\n########################################"
+
+    putStrLn "TESTING PSNODIG PARSERS\n"
+    mapM_ (\parserTest -> parserTest >>= putStrLn) testSuiteParsers
+
+    putStrLn "\n\n########################################"
+
+    putStrLn "\n\nTESTING PSNODIG WRITERS\n"
+    mapM_ (\writerTest -> writerTest >>= putStrLn) testSuiteWriters
+
+
+runTest :: String -> String -> Test -> IO String
+runTest name testPart currentTest = do
+    putStrLn $ "\n" ++ name ++ " " ++ testPart ++ ":"
+    runTestTT currentTest >>= return . show
+
+testSuiteParsers :: [IO String]
+testSuiteParsers =
+    [ runTest "Gourmet" "Parser" testGourmetParser ]
+
+testSuiteWriters :: [IO String]
+testSuiteWriters =
+    [ runTest "Gourmet" "Writer" testGourmetWriter
+    , runTest "Pytite" "Writer" testPytiteWriter
+    , runTest "Pseudocode" "Writer" testPseudocodeWriter
+    ]
